ee8224825cea63578239eb54d9fbb9f327f9ecca
mrmoish <v.y.moish@gmail.com>
2024-11-28 13:37:42 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,46 +1,18 @@
+const ModuleURL = require('url');
+
 // Экспортируем асинхронную функцию-обработчик, которая отвечает на HTTP-запросы
 // export -  функцию доступной за пределами файла.
 // export async default function handler(req, res) { 
 // не работает без default(без которого не нужно прямо указывать точное имя фунции при работе из другого файла)
-// export default function handler(req, res) {
-//   // const targetUrl = req.query.url;
-//   try {
-//     // const response = await fetch(targetUrl);
-    
-//     // if (!response.ok) {
-//     //     throw new Error(`Ошибка: ${response.status}`);
-//     // }
-//     // const data = await response.text();
-
-//     // res.send(response.data);
-//     res.status(200).json({ message: 'Hello, world!' });
-//   } catch (error) {
-//       res.status(500).send('Ошибка при выполнении запроса');
-//   }
-// });
-
-
-const ModuleURL = require('url');
-
 export default async function handler(req, res) {
-  // Метод запроса (GET, POST и т. д.)
-  const { method } = req;
 
-  switch (method) {
-    case 'GET':
-
-         
-      // Обработка GET-запроса
-      res.status(200).json({ message:  ModuleURL.parse(req.url,true).query.url });
-      break;
-    case 'POST':
-      // Обработка POST-запроса
-      const data = req.body;
-      res.status(200).json({ message: 'Data received', data });
-      break;
-    default:
-      // Метод не поддерживается
-      res.setHeader('Allow', ['GET', 'POST']);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
+    const response = await fetch('jw.org');
+    if (!response.ok) {
+        // res.status(200).json({ message:  ModuleURL.parse(req.url,true).query.url });
+        res.status(200).json({ message: 'errrrrr'});
+        // throw new Error(`Ошибка: ${response.status}`);
+    }
+    
+    res.status(200).json({ message: 'okkik'});
+      
 }
