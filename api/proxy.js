5036f235dfca676d29a85e748bd1545c7b9df61d
mrmoish <v.y.moish@gmail.com>
2024-11-28 13:20:28 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,17 +3,17 @@
 // export async default function handler(req, res) { 
 // не работает без default(без которого не нужно прямо указывать точное имя фунции при работе из другого файла)
 export default async function handler(req, res) {
-  const targetUrl = req.query.url;
+  // const targetUrl = req.query.url;
   try {
-    const response = await fetch(targetUrl);
+    // const response = await fetch(targetUrl);
     
-    if (!response.ok) {
-        throw new Error(`Ошибка: ${response.status}`);
-    }
-    const data = await response.text();
+    // if (!response.ok) {
+    //     throw new Error(`Ошибка: ${response.status}`);
+    // }
+    // const data = await response.text();
 
     // res.send(response.data);
-    res.send(data);
+    res.send('ok');
   } catch (error) {
       res.status(500).send('Ошибка при выполнении запроса');
   }
